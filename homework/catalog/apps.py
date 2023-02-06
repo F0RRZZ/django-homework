218f98e0ef1bbcc72c03eaf4e90ac65d153c5856
Marat Khagazheev <marathagazeev@gmail.com>
2023-02-06 22:34:49 +0300

    linted

--- homework/catalog/apps.py
+++ homework/catalog/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalog'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "catalog"
