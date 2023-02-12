40b94c6b92b44351b2dd5d64f91bb3ef2da4b0b2
Marat Khagazheev <marathagazeev@gmail.com>
2023-02-12 08:11:12 +0300

    Linted

--- homework/catalog/urls.py
+++ homework/catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.item_list),
-    path('<int:item>/', views.item_detail)
+    path('<int:item>/', views.item_detail),
 ]
--- homework/homework/urls.py
+++ homework/homework/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
