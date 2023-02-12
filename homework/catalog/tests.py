e2127b53fd0527247cb870f177fad26ae6662cd0
Marat Khagazheev <marathagazeev@gmail.com>
2023-02-12 15:10:02 +0300

    Add positive integer tests

--- homework/catalog/tests.py
+++ homework/catalog/tests.py
@@ -20,13 +20,13 @@ class CatalogPageTests(TestCase):
             response.status_code, 404, 'Invalid product link worked'
         )
 
-    def test_right_regex(self):
+    def test_right_link_with_regex(self):
         response = Client().get('/catalog/re/1/')
         self.assertEqual(
             response.status_code, 200, 'Error with the right regex'
         )
 
-    def test_wrong_regex(self):
+    def test_wrong_link_with_regex(self):
         response_negative_number = Client().get('/catalog/re/-1/')
         self.assertEqual(
             response_negative_number.status_code,
@@ -37,4 +37,23 @@ class CatalogPageTests(TestCase):
         response_str = Client().get('/catalog/re/a/')
         self.assertEqual(
             response_str.status_code, 404, 'Regex with string worked'
+        )
+
+    def test_right_link_with_positive_number(self):
+        response = Client().get('/catalog/converter/1')
+        self.assertEqual(
+            response.status_code, 200, 'Error with the positive number'
+        )
+
+    def test_wrong_link_with_positive_number(self):
+        response_negative_number = Client().get('/catalog/converter/-1')
+        self.assertEqual(
+            response_negative_number.status_code,
+            404,
+            'Link with negative number worked'
+        )
+
+        response_str = Client().get('/catalog/converter/a')
+        self.assertEqual(
+            response_str.status_code, 404, 'Link with string worked'
         )
