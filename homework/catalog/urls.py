2dc5eb92532ced2cc688f953f0957f29dd63acac
Marat Khagazheev <marathagazeev@gmail.com>
2023-02-14 18:33:36 +0300

    Edit catalog urls 2

--- homework/catalog/urls.py
+++ homework/catalog/urls.py
@@ -7,14 +7,14 @@ register_converter(converters.PositiveInteger, 'posint')
 app_name = 'catalog'
 urlpatterns = [
     path('', views.item_list, name='item-list'),
-    path('<int:item>/', views.item_detail, name='item-detail'),
+    path('<int:pk>/', views.item_detail, name='item-detail'),
     path(
-        'converter/<posint:number>',
+        'converter/<posint:pk>',
         views.item_detail,
         name='postitive-number-check',
     ),
     re_path(
-        r're/(?P<number>[1-9]\d*)/$',
+        r're/(?P<pk>[1-9]\d*)/$',
         views.item_detail,
         name='re-positive-number-check',
     ),
