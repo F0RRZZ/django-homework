c507103ac5a7d99ba2a077b33adb7d31eb6d8413
Marat Khagazheev <marathagazeev@gmail.com>
2023-02-16 15:21:18 +0300

    Edit middleware tests

--- homework/homepage/views.py
+++ homework/homepage/views.py
@@ -2,9 +2,11 @@ from http import HTTPStatus
 
 from django.http import HttpResponse
 
+homepage_title = 'Главная'
+
 
 def home(request):
-    return HttpResponse('Главная')
+    return HttpResponse(homepage_title)
 
 
 def teapot_status_page(request):
--- homework/homework/middleware/tests.py
+++ homework/homework/middleware/tests.py
@@ -1,40 +1,45 @@
 from http import HTTPStatus
 
-from django.test import Client, TestCase, override_settings
-
-from homework.middleware.russian_words_reverser import reverse_russian_words
+from django.test import TestCase, override_settings
+from homepage import views
 
 
 class CustomMiddlewareTests(TestCase):
+    @override_settings(RUSSIAN_WORDS_REVERSING_MIDDLEWARE_ENABLED=False)
     def test_is_middleware_correctly_working(self):
-        with override_settings(
-            RUSSIAN_WORDS_REVERSING_MIDDLEWARE_ENABLED=False
-        ):
-            client = Client()
-            response = client.get('/')
-            self.assertEqual(response.status_code, HTTPStatus.OK)
-            original_text = response.content.decode('utf-8')
-            reversed_text = original_text
-            for _ in range(20):
-                response = client.get('/')
-                text = response.content.decode('utf-8')
-                if text != original_text:
-                    reversed_text = text
-                    break
-            self.assertEquals(original_text, reversed_text)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        original_text = response.content.decode('utf-8')
+        reversed_text = original_text
+        for _ in range(10):
+            response = self.client.get('/')
+            text = response.content.decode('utf-8')
+            if text != original_text:
+                reversed_text = text
+                break
+        self.assertEquals(original_text, reversed_text)
 
+    @override_settings(RUSSIAN_WORDS_REVERSING_MIDDLEWARE_ENABLED=True)
     def test_is_content_reversing(self):
-        with override_settings(
-            RUSSIAN_WORDS_REVERSING_MIDDLEWARE_ENABLED=True
-        ):
-            client = Client()
-            response = client.get('/')
-            original_text = response.content.decode('utf-8')
-            reversed_text = ''
-            for _ in range(20):
-                response = client.get('/')
-                text = response.content.decode('utf-8')
-                if text != original_text:
-                    reversed_text = reverse_russian_words(text)
-                    break
-            self.assertEqual(original_text, reversed_text)
+        cases = [
+            ('/', '!стр124ок[a]', '!ртс124ко[a]'),
+            ('/', '1287!*#&874198274#!', '1287!*#&874198274#!'),
+            ('/', 'hello', 'hello'),
+            ('/', ' ', ' '),
+            ('/', 'шdшdшdшdшd', 'шdшdшdшdшd'),
+            ('/', 'ГлаВнаЯ', 'ЯанВалГ'),
+            ('/', 'helloпривет', 'helloтевирп'),
+        ]
+        for case in cases:
+            views.homepage_title = case[1]
+            result = []
+            for _ in range(10):
+                response = self.client.get(case[0])
+                result.append(response.content.decode('utf-8'))
+            self.assertIn(
+                case[2],
+                result,
+                f'\nRussian words reversing middleware'
+                f' did not give the correct result'
+                f'(case: {case[1]}; expected: {case[2]})',
+            )
