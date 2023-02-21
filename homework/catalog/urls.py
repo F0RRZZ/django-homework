61591e1784ba6887961d6b8245b3a55c7ea922d3
Marat Khagazheev <marathagazeev@gmail.com>
2023-02-21 20:50:41 +0300

    Edit imports in catalog/urls

--- homework/catalog/urls.py
+++ homework/catalog/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, re_path, register_converter
 from catalog import converters, views
+from django.urls import path, re_path, register_converter
 
 register_converter(converters.PositiveInteger, 'posint')
 
