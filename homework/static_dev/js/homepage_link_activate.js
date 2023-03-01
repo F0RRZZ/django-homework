913a86f8e47152985a255f2af0562a40d6b140d9
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-01 19:36:21 +0300

    homepage_link_activate.js optimized

--- homework/static_dev/js/homepage_link_activate.js
+++ homework/static_dev/js/homepage_link_activate.js
@@ -1,14 +1,8 @@
 const activePage = window.location.pathname;
 document.querySelectorAll('a').
 forEach(link => {
-    if (link.href.includes(`${activePage}`) && activePage != '/') {
+    if (link.href.includes(`${activePage}`)) {
         link.classList.add('active');
         link.classList.add('disabled');
-        link
-    } else if (activePage == '/') {
-        if (link.href == 'http://127.0.0.1:8000/'){
-            link.classList.add('active');
-            link.classList.add('disabled');
         }
-    }
 });
