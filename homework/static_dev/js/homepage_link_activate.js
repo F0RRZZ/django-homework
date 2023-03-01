76c04782385b2c87229271d21911123dd7c53f4d
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-01 21:47:09 +0300

    Translator bug fix

--- homework/static_dev/js/homepage_link_activate.js
+++ homework/static_dev/js/homepage_link_activate.js
@@ -1,8 +1,17 @@
 const activePage = window.location.pathname;
 document.querySelectorAll('a').
 forEach(link => {
-    if (link.href.includes(`${activePage}`)) {
-        link.classList.add('active');
-        link.classList.add('disabled');
+    if (activePage != '/') {
+        if (link.href != 'http://127.0.0.1:8000/') {
+            if (link.href.includes(`${activePage}`)) {
+                link.classList.add('active');
+                link.classList.add('disabled');
+            }
         }
+    } else {
+        if (link.href == 'http://127.0.0.1:8000/') {
+            link.classList.add('active');
+            link.classList.add('disabled');
+        }
+    }
 });
