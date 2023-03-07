9739f6576079b0e5aa05b00058cfa7ecc418dd77
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-07 10:39:36 +0300

    Add test_views

--- homework/catalog/tests/tests_views.py
+++ homework/catalog/tests/tests_views.py
@@ -1,11 +1,10 @@
-from http import HTTPStatus
-
 from django.test import Client, TestCase
+import django.urls
 
 import catalog.models
 
 
-class CatalogPageTests(TestCase):
+class CatalogViewsTests(TestCase):
     def setUp(self) -> None:
         published_category = catalog.models.Category.objects.create(
             name='published_category',
@@ -55,41 +54,20 @@ class CatalogPageTests(TestCase):
         unpublished_item.clean()
         unpublished_item.save()
 
-    def test_catalog_endpoint(self):
+    def test_home_page_show_correct_context(self):
+        response = Client().get(django.urls.reverse('homepage:index'))
+        self.assertIn('items', response.context)
+
+    def test_home_count_item(self):
+        response = Client().get(django.urls.reverse('homepage:index'))
+        items = response.context['items']
+        self.assertEqual(items.count(), 1)
+
+    def test_catalog_show_correct_context(self):
         response = Client().get('/catalog/')
-        self.assertEqual(
-            response.status_code,
-            HTTPStatus.OK,
-            'Error when going to the page "catalog"',
-        )
+        self.assertIn('items', response.context)
 
-    def test_catalog_item_endpoint(self):
-        response = Client().get('/catalog/1/')
-        self.assertEqual(
-            response.status_code,
-            HTTPStatus.OK,
-            'Error with the right type of item (/catalog/1/)',
-        )
-
-    def test_catalog_item_positive_integer_endpoint(self):
-        urls = ['/catalog/re/{}/', '/catalog/converter/{}']
-        cases = [
-            ('1', HTTPStatus.OK),
-            ('0', HTTPStatus.NOT_FOUND),
-            ('-1', HTTPStatus.NOT_FOUND),
-            ('010', HTTPStatus.NOT_FOUND),
-            ('1.0', HTTPStatus.NOT_FOUND),
-            ('1a', HTTPStatus.NOT_FOUND),
-            ('a', HTTPStatus.NOT_FOUND),
-            ('^1', HTTPStatus.NOT_FOUND),
-            ('aa1', HTTPStatus.NOT_FOUND),
-            ('z1x', HTTPStatus.NOT_FOUND),
-            ('^1.0', HTTPStatus.NOT_FOUND),
-        ]
-        for url in urls:
-            for case in cases:
-                test_url = url.format(case[0])
-                response = Client().get(test_url)
-                self.assertEqual(
-                    response.status_code, case[1], f'(URL: {test_url})'
-                )
+    def test_catalog_count_item(self):
+        response = Client().get('/catalog/')
+        items = response.context['items']
+        self.assertEqual(items.count(), 2)
