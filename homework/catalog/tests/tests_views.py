8d890a23a966b21655c13c327c351ec6037d2f86
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-08 11:12:36 +0300

    Edit tests

--- homework/catalog/tests/tests_views.py
+++ homework/catalog/tests/tests_views.py
@@ -5,69 +5,24 @@ import catalog.models
 
 
 class CatalogViewsTests(TestCase):
-    def setUp(self) -> None:
-        published_category = catalog.models.Category.objects.create(
-            name='published_category',
-            slug='published_category',
-        )
-        unpublished_category = catalog.models.Category.objects.create(
-            name='unpublished_category',
-            slug='unpublished_category',
-            is_published=False,
-        )
-        published_tag = catalog.models.Tag.objects.create(
-            name='published_tag',
-            slug='published_tag',
-        )
-        unpublished_tag = catalog.models.Tag.objects.create(
-            name='unpublished_tag',
-            slug='unpublished_tag',
-            is_published=False,
-        )
-        published_item_on_main = catalog.models.Item.objects.create(
-            name='published_item_on_main',
-            text='роскошно',
-            category=published_category,
-            is_on_main=True,
-        )
-        published_item_not_on_main = catalog.models.Item.objects.create(
-            name='published_item_not_on_main',
-            text='роскошно',
-            category=published_category,
-        )
-        unpublished_item = catalog.models.Item.objects.create(
-            name='unpublished_item',
-            text='роскошно',
-            category=unpublished_category,
-        )
-
-        published_category.save()
-        unpublished_category.save()
-
-        published_tag.save()
-        unpublished_tag.save()
-
-        published_item_on_main.clean()
-        published_item_on_main.save()
-        published_item_not_on_main.clean()
-        published_item_not_on_main.save()
-        unpublished_item.clean()
-        unpublished_item.save()
+    fixtures = ['data.json']
 
     def test_home_page_show_correct_context(self):
         response = Client().get(django.urls.reverse('homepage:index'))
         self.assertIn('items', response.context)
+        self.assertNotIn('categories', response.context)
 
     def test_home_count_item(self):
         response = Client().get(django.urls.reverse('homepage:index'))
         items = response.context['items']
-        self.assertEqual(len(items), 1)
+        self.assertEqual(len(items), 2)
 
     def test_catalog_show_correct_context(self):
         response = Client().get('/catalog/')
         self.assertIn('categories', response.context)
+        self.assertNotIn('items', response.context)
 
     def test_catalog_count_categories(self):
         response = Client().get('/catalog/')
         categories = response.context['categories']
-        self.assertEqual(len(categories), 1)
+        self.assertEqual(len(categories), 2)
