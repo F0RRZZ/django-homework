fa1564c5a5d664477fff747c7d7bcf50124aa4f9
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-08 22:40:14 +0300

    Bug fix

--- homework/static_dev/js/links_activator.js
+++ homework/static_dev/js/links_activator.js
@@ -3,7 +3,7 @@ document.querySelectorAll('a').
 forEach(link => {
     if (activePage != '/') {
         if (link.href != 'http://127.0.0.1:8000/' && link.href != 'http://127.0.0.1:8000/admin/') {
-            if (link.href.includes(`${activePage}`)) {
+            if (link.href.endsWith(`${activePage}`)) {
                 link.classList.add('active');
                 link.classList.add('disabled');
             }
