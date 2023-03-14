578e9528b8dda01052d641ac558a248529367881
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-14 19:24:57 +0300

    Add test urls

---
+++ homework/feedback/tests/test_urls.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+
+class FeedbackPageTests(TestCase):
+    def test_feedback_endpoint(self):
+        response = Client().get('/feedback/')
+        self.assertEqual(
+            response.status_code,
+            HTTPStatus.OK,
+            'Error when going to the page "feedback"'
+        )
