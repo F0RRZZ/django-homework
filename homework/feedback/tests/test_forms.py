93227204fd16bfe8f2c6cdf7cbe89c25c017d582
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-20 22:11:20 +0300

    Feedback tests replaced

--- homework/feedback/tests/test_forms.py
+++ homework/feedback/tests/test_forms.py
@@ -1,10 +1,5 @@
-import os
+from django.test import TestCase
 
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import Client, TestCase
-from django.urls import reverse
-
-import feedback.models
 import feedback.forms
 
 
@@ -31,46 +26,3 @@ class FormTests(TestCase):
         self.assertEqual(
             email_help_text, 'введите почту, на которую будет отправлен ответ'
         )
-
-    def test_create_feedback(self):
-        feedbacks_count = feedback.models.Feedback.objects.count()
-        form_data = {'text': 'text', 'email': 'ex@ex.com'}
-        Client().post(
-            reverse('feedback:feedback'),
-            data=form_data,
-            follow=True,
-        )
-        self.assertEqual(
-            feedback.models.Feedback.objects.count(), feedbacks_count + 1
-        )
-        self.assertTrue(
-            feedback.models.Feedback.objects.filter(text='text').exists()
-        )
-
-    def test_redirect(self):
-        form_data = {'text': 'text', 'email': 'ex@ex.com'}
-        response = Client().post(
-            reverse('feedback:feedback'),
-            data=form_data,
-            follow=True,
-        )
-        self.assertRedirects(response, reverse('feedback:success'))
-
-    def test_file_upload(self):
-        with open('test_file.txt', 'w') as f:
-            f.write('test')
-        with open('test_file.txt', 'rb') as f:
-            form_data = {
-                'text': 'text',
-                'email': 'ex@ex.com',
-                'files': SimpleUploadedFile(f.name, f.read()),
-            }
-            Client().post(
-                reverse('feedback:feedback'),
-                data=form_data,
-                follow=True,
-            )
-            self.assertTrue(os.path.exists('uploads/1/test_file.txt'))
-        os.remove('uploads/1/test_file.txt')
-        os.remove('test_file.txt')
-        os.rmdir('uploads/1/')
