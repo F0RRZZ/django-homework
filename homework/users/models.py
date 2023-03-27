6f0d6d5f5346b085ea790d2b31e2f7cd7f19a241
Marat Khagazheev <marathagazeev@gmail.com>
2023-03-27 11:20:41 +0300

    Edit verbose names

--- homework/users/models.py
+++ homework/users/models.py
@@ -83,11 +83,6 @@ class UserProfile(
     birthday = models.DateTimeField(
         _('день рождения'), null=True, blank=True, help_text='День рождения'
     )
-
-    def get_image_filename(self, filename):
-        ext = os.path.splitext(filename)[-1]
-        return 'avatars/user_{}{}'.format(self.id, ext)
-
     image = models.ImageField(
         _('аватарка'),
         upload_to=get_image_filename,
@@ -108,8 +103,12 @@ class UserProfile(
     objects = UserProfileManager()
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def get_image_filename(self, filename):
+        ext = os.path.splitext(filename)[-1]
+        return 'avatars/user_{}{}'.format(self.id, ext)
 
     def natural_key(self):
         return self.username
